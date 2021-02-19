156e65cf1be6b142034599c27f628a4a5072355c
Ravi-Papasani <76274650+Ravi-Papasani@users.noreply.github.com>
2021-02-18 21:22:25 -0600

    display no tasks text message, if the tasks are empty

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const deleteTask = (id) => {
   return (
     <div className="container">
       <Header/>
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      { tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask}/>) : ('No tasks to display')}
     </div>
   );
 }
