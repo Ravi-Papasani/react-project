325db6c373fbd9e6a30e44747581abb64b63f30b
Ravi-Papasani <76274650+Ravi-Papasani@users.noreply.github.com>
2021-02-27 00:29:13 -0600

    if the url is / then only show Add button in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
 import Button from "./Button";
 
 const Header = ({title, onAdd, showAdd}) => {
     const onClickEvent = () => {
         console.log('Button clicked');
     }
+
+    const location = useLocation();
+
     return (
         <div>
             <h1 className='header'>{title}</h1>
-            <Button 
+            {location.pathname === '/' && <Button 
             text={showAdd ? 'Close' : 'Add'} 
             color={showAdd ? 'red' : 'green'} 
-            onClick={onAdd}/>
+            onClick={onAdd}/>}
         </div>
     )
 }
